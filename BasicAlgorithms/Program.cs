c4aa49d7889cfcea90e54522d4650443098a6e59
Phu.Nguyen <Phu.Nguyen@altsrc.net>
2019-10-14 18:04:05 +0700

    LongestMadeUpString

--- BasicAlgorithms/Program.cs
+++ BasicAlgorithms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BasicAlgorithms
 {
@@ -7,7 +8,10 @@ namespace BasicAlgorithms
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine(nameof(Program.FindPairWithSum));
 			Console.WriteLine(FindPairWithSum(new int[] { 1, 5, 6, 9 }, 8));
+			Console.WriteLine(nameof(Program.CheckDuplicateStringStream));
+			CheckDuplicateStringStream(new string[] {"abc", "sadsa", "werew"});
 		}
 
 		/// <summary>
@@ -19,7 +23,7 @@ namespace BasicAlgorithms
 		/// <returns></returns>
 		static bool FindPairWithSum(int[] data, int sum)
 		{
-			HashSet<int> complements = new HashSet<int>();
+			var complements = new HashSet<int>();
 			foreach (var value in data)
 			{
 				if (complements.Contains(sum - value))
@@ -30,6 +34,66 @@ namespace BasicAlgorithms
 			}
 
 			return false;
+		}
+		/// <summary>
+		///Input: arr[] = {“geeks”, “for”, “geeks”}
+		///Output:
+		///No
+		///	No
+		///Yes
+
+		///	Input: arr[] = {“abc”, “aaa”, “cba”}
+		///Output:
+		///No
+		///	No
+		///No
+		/// </summary>
+		/// <param name="data"></param>
+		static void CheckDuplicateStringStream(string[] data)
+		{
+			var foundStrings = new HashSet<string>();
+			foreach (var s in data)
+			{
+				if (foundStrings.Contains(s))
+				{
+					Console.WriteLine("Yes");
+				}
+				else
+				{
+					foundStrings.Add(s);
+					Console.WriteLine("No");
+				}
+			}
+		}
+
+		/// <summary>
+		/// Input: arr[] = {“geeks”, “for”, “geeksfor”, “geeksforgeeks”}
+		///Output: geeksforgeeks
+		///“geeksforgeeks” is made up of(“geeks” + “for” + “geeks”).
+		///Even though “geeksfor” is also made up of other strings
+		///but it is not the largest string.
+		/// 
+		///Input: arr[] = {“Hey”, “you”, “stop”, “right”, “there”}
+		///Output : -1
+		/// </summary>
+		/// <param name="data"></param>
+		static void LongestMadeUpString(string[] data)
+		{
+			var sortedBasedOnLength = data.OrderByDescending(s => s.Length).ToArray();
+			var stringMap = sortedBasedOnLength.ToHashSet();
+			foreach (var s in sortedBasedOnLength)
+			{
+				
+			}
+		}
+
+		static bool CanBeBuiltFromOtherElements(string[] data, int currentIndex, HashSet<string> stringMap)
+		{
+			var currentString = data[currentIndex];
+			for (int i = 0; i < data.Length; i++)
+			{
+				if (i != currentIndex && currentString.)
+			}
 		}
 	}
 }
